8ddb4ecdde33a541d316b52973ab1dd2ee2dd082
davidperlera2 <00165220@uca.edu.sv>
2024-11-27 11:07:06 -0600

    add: Ejemplo

--- source/ExampleC.c
+++ source/ExampleC.c
@@ -1,12 +1,30 @@
-while (count < 10){
-    if(5){
-        
-    }else if (5>variable && 1+2+3 < 6){
-
-    }else if (test){
-
-    }else{
-        
-    }
+int calculateSum(int a, int b) {
+    return a + b;
 }
 
+int main() {
+    // Manejo de variables
+    int num1 = 5;
+    float average = 0.0;
+    char grade = 'A';
+
+    // Operadores aritméticos
+    int sum = num + num;
+    int difference = num2 - num1;
+    int product = num1 * num2;
+
+    // Condicional if-else
+    if (isGreater) {
+        num = num - 1;
+    } else {
+        num = num + 1;
+    }
+
+    // Iteración for
+    for (int i = 0; i < 5; i=i+1) {
+        // DO STUFF
+    }
+
+    // Retorno
+    return 0;
+}
